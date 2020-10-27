65297b0c2b6b530d6ea1d834371cd7dbd4d55388
mojyraam <mojyraam@gmail.com>
2020-10-27 11:11:07 +0330

    v_1

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import theme from './ui/Theme';
+import Header from './ui/Header';
+import Footer from './ui/Footer';
+import LandingPage from '../components/LandingPage';
+import Services from './Services';
+
+function App() {
+
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [value, setValue] = useState(0)
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+        <Switch>
+          <Route exact path="/" render={ (props) => <LandingPage { ...props } setValue={ setValue } setSelectedIndex={ setSelectedIndex } /> } />
+          <Route exact path="/services" render={ (props) => <Services { ...props } setValue={ setValue } setSelectedIndex={ setSelectedIndex } /> } />
+          <Route exact path="/customsoftware" component={ () => <div>Custom Software</div> } />
+          <Route exact path="/mobileapps" component={ () => <div>Mobile Apps</div> } />
+          <Route exact path="/websites" component={ () => <div>Websites</div> } />
+          <Route exact path="/revolution" component={ () => <div>The Revolution</div> } />
+          <Route exact path="/about" component={ () => <div>About Us</div> } />
+          <Route exact path="/contact" component={ () => <div>Contact Us</div> } />
+          <Route exact path="/estimate" component={ () => <div>Estimate</div> } />
+        </Switch>
+        <Footer setValue={setValue} setSelectedIndex={setSelectedIndex} />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
